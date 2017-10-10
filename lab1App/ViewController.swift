394941e0729bd21dda2549a5463a5b337ba9b958
Użytkownik Gość <Guest@imac-05.local>
2017-10-10 09:28:02 +0200

    fetching data try

--- lab1App/ViewController.swift
+++ lab1App/ViewController.swift
@@ -13,13 +13,29 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var url = URL(string: "http://borg.kis.agh.edu.pl/~sebi/albums.php")
+        
+        let urlSession = URLSession.shared
+        
+        var request : URLRequest = URLRequest(url: url!)
+        
+        let dataTask = urlSession.dataTask(with: request, completionHandler: handleData){
+            
+        }
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    func handleData(data: Data?, response: URLResponse?, error: Error?) -> Void{
+        
+        
+    }
+    
 
 }
 
